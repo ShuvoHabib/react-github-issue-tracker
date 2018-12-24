ea770545b7a61ce69792ba1235afd76443b276ec
shuvo habib <shuvohabib@gmail.com>
2018-12-24 17:49:17 +0600

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import configureStore from './store';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, IndexRedirect, browserHistory, applyRouterMiddleware } from 'react-router';
+import useScroll from 'react-router-scroll/lib/useScroll';
+import rootReducer from './redux/reducers';
+import App from './containers/App';
+import IssueListPage from './containers/IssueListPage';
+import IssueDetailPage from './containers/IssueDetailPage';
 import './index.css';
-import App from './App';
+
+let store = createStore(rootReducer, applyMiddleware(thunk));
+
+const routes = (
+  <Router history={browserHistory} render={applyRouterMiddleware(useScroll())}>
+    <Route path="/" component={App}>
+      <IndexRedirect to="/facebook/react/issues"/>
+      <Route path="/:org/:repo/issues">
+        <IndexRoute component={IssueListPage}/>
+        <Route path=":issueId" component={IssueDetailPage}/>
+      </Route>
+    </Route>
+  </Router>
+);
+
 ReactDOM.render(
-	<Provider store={configureStore()}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
+  <Provider store={store}>
+    {routes}
+  </Provider>,
+  document.getElementById('root')
 );
